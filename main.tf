391fda5054d404e26fa660e0870baccf8c29b5d0
Sammreen Amir <96933719+sammreenamir@users.noreply.github.com>
2024-11-20 02:03:45 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,8 @@
-provider "google" {
-  project = "cloud-resume-sammreen"
-  region  = "us-central1"
-}
-
-resource "google_cloudfunctions_function" "visitor_counter" {
-  name                  = "count-func02"             
-  runtime               = "python311"                 
-  entry_point           = "visitor_counter"           
+resource "google_cloudfunctions_function" "my_function" {
+  name        = "cloud-resume-function"
+  description = "My Cloud Resume Function"
+  runtime     = "python311"  # Ensure it matches your Python environment
   source_archive_bucket = "sammreenamir.online"
-  source_archive_object = "cloud-resume-terraform.zip" 
-  trigger_http          = true                        
+  source_archive_object = "cloud-resume-terraform.zip"
+  entry_point = "visitor_counter"  # This must match the function name in main.py
 }
