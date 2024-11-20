7100f3c2d9b85f4cd2a0b0c8b1c87dfa70e5ee36
Sammreen Amir <96933719+sammreenamir@users.noreply.github.com>
2024-11-20 10:29:51 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
-resource "google_cloudfunctions_function" "visitor_counter" {
-  name        = "visitorCounterFunction2"
-  runtime     = "python311"
-  region      = "us-central1"
-  project     = "cloud-resume-sammreen"
-  entry_point = "visitor_counter"
-
-  source_archive_bucket = "sammreenamir.online"
-  source_archive_object = "cloud-resume-terraform.zip"
-
-  environment_variables = {
-    "GOOGLE_FUNCTION_SOURCE" = "main.py"
-  }
-
-  trigger_http = true
+provider "google" {
+  project = "cloud-resume-sammreen"
+  region  = "us-central1"
 }
 
+resource "google_cloudfunctions_function" "visitor_counter" {
+  name                  = "count-func02"             
+  runtime               = "python311"                 
+  entry_point           = "visitor_counter"           
+  source_archive_bucket = "sammreenamir.online"
+  source_archive_object = "cloud-resume-terraform.zip" 
+  trigger_http          = true                        
+}
